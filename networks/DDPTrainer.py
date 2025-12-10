5c1ac8f8e7f20513fb2d41b4e9e64b3ee0f5ae19
ZackRibeiro <ZackRibeiro@gmx.fr>
2025-12-10 09:13:35 +0100

    101225

--- networks/DDPTrainer.py
+++ networks/DDPTrainer.py
@@ -14,7 +14,7 @@ class DDPTrainer(Trainer):
     Extension of Trainer class to train Denoising Diffusion Probabilistic Models.
     """
     def __init__(self, *args,
-                 timesteps:int=1000, beta_schedule:Literal["linear","cosine","quadratic"]="linear", beta_start:float=1e-4, beta_end:float=0.02,
+                 timesteps:int=500, beta_schedule:Literal["linear","cosine","quadratic"]="linear", beta_start:float=1e-4, beta_end:float=0.02,
                   **kwargs):
         """
         To know args and variables, please refers to '~.Trainer.__init__'
@@ -33,7 +33,7 @@ class DDPTrainer(Trainer):
             LOGGER.error(beta_schedule+" is not an option.")
             raise ValueError("Beta schedule incorrect")
         
-        self.betas = betas.float() #(T,)
+        self.betas = betas.float().to(self.device) #(T,)
         self.alphas = 1.-self.betas #(T,)
         self.alphas_cumprod = torch.cumprod(self.alphas, dim=0) #(T,)
         self.alphas_cumprod_prev = F.pad(self.alphas_cumprod[:-1], (1,0), value=1.) # (T,)
@@ -89,39 +89,40 @@ class DDPTrainer(Trainer):
         B, C, H, W = input.shape
         device = self.device
 
-        x_t = torch.randn((B, C, H, W), device=device)
+        with torch.no_grad(): 
+            x_t = torch.randn((B, C, H, W), device=device)
 
-        for time_step in reversed(range(self.timesteps)):
-            t = torch.full((B,), time_step, device=device, dtype=torch.long)
+            for time_step in reversed(range(self.timesteps)):
+                t = torch.full((B,), time_step, device=device, dtype=torch.long)
 
-            eps_pred = model(torch.cat([x_t, input], dim=1), t)
+                eps_pred = model(torch.cat([x_t, input], dim=1), t)
 
-            sqrt_ac = self.sqrt_alphas_cumprod[t].view(B, 1, 1, 1)
-            sqrt_omac = self.sqrt_one_minus_alphas_cumprod[t].view(B, 1, 1, 1)
-            x0_pred = (x_t - sqrt_omac * eps_pred) / (sqrt_ac + 1e-8)
+                sqrt_ac = self.sqrt_alphas_cumprod[t].view(B, 1, 1, 1)
+                sqrt_omac = self.sqrt_one_minus_alphas_cumprod[t].view(B, 1, 1, 1)
+                x0_pred = (x_t - sqrt_omac * eps_pred) / (sqrt_ac + 1e-8)
 
-            x0_pred = x0_pred.clamp(-1.0, 1.0)
+                x0_pred = x0_pred.clamp(-1.0, 1.0)
 
-            if time_step == 0:
-                x_t = x0_pred
-                break
+                if time_step == 0:
+                    x_t = x0_pred
+                    break
 
-            beta_t      = self.betas[t].view(B, 1, 1, 1)
-            alpha_t     = self.alphas[t].view(B, 1, 1, 1)
-            alpha_bar_t = self.alphas_cumprod[t].view(B, 1, 1, 1)
-            alpha_bar_prev = self.alphas_cumprod_prev[t].view(B, 1, 1, 1)
-            posterior_var = self.posterior_variance[t].view(B, 1, 1, 1)
+                beta_t      = self.betas[t].view(B, 1, 1, 1)
+                alpha_t     = self.alphas[t].view(B, 1, 1, 1)
+                alpha_bar_t = self.alphas_cumprod[t].view(B, 1, 1, 1)
+                alpha_bar_prev = self.alphas_cumprod_prev[t].view(B, 1, 1, 1)
+                posterior_var = self.posterior_variance[t].view(B, 1, 1, 1)
 
-            coef1 = (beta_t * torch.sqrt(alpha_bar_prev)) / (1.0 - alpha_bar_t)
-            coef2 = (torch.sqrt(alpha_t) * (1.0 - alpha_bar_prev)) / (1.0 - alpha_bar_t)
-            posterior_mean = coef1 * x0_pred + coef2 * x_t
+                coef1 = (beta_t * torch.sqrt(alpha_bar_prev)) / (1.0 - alpha_bar_t)
+                coef2 = (torch.sqrt(alpha_t) * (1.0 - alpha_bar_prev)) / (1.0 - alpha_bar_t)
+                posterior_mean = coef1 * x0_pred + coef2 * x_t
 
-            noise = torch.randn_like(x_t)
-            x_t = posterior_mean + torch.sqrt(posterior_var.clamp(min=1e-20)) * noise
+                noise = torch.randn_like(x_t)
+                x_t = posterior_mean + torch.sqrt(posterior_var.clamp(min=1e-20)) * noise
 
-            x_t = x_t.clamp(-1.5, 1.5)
+                x_t = x_t.clamp(-1.0, 1.0)
 
-        return x_t.clamp(-1.0, 1.0)
+            return x_t.clamp(-1.0, 1.0)
     
     @staticmethod
     def load(model_name, load_model=True):
@@ -131,8 +132,11 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     from POLARIScore.objects.Dataset import getDataset
     from POLARIScore.config import DATA_NORMALIZATION_CDENS, DATA_NORMALIZATION_VDENS, DATA_NORMALIZATION_CDENS_TORCH, DATA_NORMALIZATION_VDENS_TORCH
-    ds1 = getDataset("batch_training")
-    ds2 = getDataset("batch_validation")
+    #ds1 = getDataset("batch_training")
+    #ds2 = getDataset("batch_validation")
+
+    ds = getDataset("batch_highres_2")
+    ds1, ds2 = ds.split(0.8)
 
     def classic_log_mse(output, target):
         output_phys = DATA_NORMALIZATION_VDENS_TORCH[1](output)
@@ -143,19 +147,18 @@ if __name__ == "__main__":
         return mse
 
 
-    trainer = DDPTrainer(DDPMUnet, ds1, ds2, model_name="DDPM", timesteps=1000, beta_schedule='linear')
+    trainer = DDPTrainer(DDPMUnet, ds1, ds2, model_name="DDPM", timesteps=500, beta_schedule='linear')
     #trainer = load_trainer("DDPM", trainer_class=DDPTrainer)
     trainer.norms = {
         "cdens": DATA_NORMALIZATION_CDENS,
         "vdens": DATA_NORMALIZATION_VDENS,
     }
-    #ds = getDataset("batch_highres_2")
-    #ds1, ds2 = ds.split(0.8)
+
     #trainer.get_validation_error()
 
-    #trainer.ema = True
+    trainer.ema = True
     trainer.validation_loss_method = classic_log_mse
-    #trainer.ema_warmup = 500
+    trainer.ema_warmup = 500
     trainer.learning_rate = 1e-3
     trainer.training_set = ds1
     trainer.validation_set = ds2
@@ -168,7 +171,7 @@ if __name__ == "__main__":
     trainer.auto_save = 250
     trainer.scheduler = ReduceLROnPlateau(trainer.optimizer, 'min', patience=10, factor=0.1, threshold=0.0001)
     trainer.init()
-    trainer.train(500,batch_number=8,compute_validation=100,early_stopping=False)
+    trainer.train(1000,batch_number=8,compute_validation=250,early_stopping=False)
     trainer.save()
 
     trainer.plot(save=True)
